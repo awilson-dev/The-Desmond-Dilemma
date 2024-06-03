31d6705778a6f4aeeb340e47f8b8969c52b9796f
Allen Wilson <awilson-dev@tech-center.com>
2024-06-03 18:18:12 -0400

    Initial Commit

---
+++ The Desmond Dilemma/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  The Desmond Dilemma
+//
+//  Created by Allen Wilson on 6/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ The Desmond Dilemma/The_Desmond_DilemmaApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_Desmond_DilemmaApp.swift
+//  The Desmond Dilemma
+//
+//  Created by Allen Wilson on 6/3/24.
+//
+
+import SwiftUI
+
+@main
+struct The_Desmond_DilemmaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
